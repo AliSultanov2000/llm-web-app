107632f640005cc09e915d0473f08068703f6d83
= <=>
2024-08-12 01:27:30 +0300

    add django changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("polls.urls")),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]

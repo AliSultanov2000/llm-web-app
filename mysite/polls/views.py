838e1cf1e3b1666a44ed4ed14f18fa120b84d46d
= <=>
2024-08-17 02:12:28 +0300

    add fills

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hi there! My name is Ali!")
+
+
+def leo(request):
+    return HttpResponse("Ты настоящий лев, вацок!")
+
+
+def wolf(request):
+    return HttpResponse("Ты настоящий волк, вацок!")

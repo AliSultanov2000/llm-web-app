8ca58c11eafe9c236db72dcb3a046bcc180491ef
= <=>
2024-08-17 23:34:07 +0300

    change name

--- project/project/settings.py
+++ project/project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
